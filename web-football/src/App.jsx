d76edc35be05246df1e1e7fb40a8142d5efc7351
epicgdog <theepicgdog@gmail.com>
2023-09-02 19:05:16 -0700

    implemented api call

--- web-football/src/App.jsx
+++ web-football/src/App.jsx
@@ -1,14 +1,29 @@
-import { useState } from 'react'
+import { useState, useEffect, Suspense } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Item from "./item.jsx"
+
+let tempArr = []
+for (let i = 0; i <= 19; i++){
+  tempArr[i] = i + 1
+}
+const TEST_ARR = tempArr
 
 function App() {
 
+  const [standings, setStandings] = useState( [] )
+
+  useEffect(() => {
+    fetch("https://api-football-standings.azharimm.dev/leagues/eng.1/standings?season=2023&sort=asc").then( res => res.json() ).then( data => { console.log(data);setStandings(data.data.standings) })
+  }, [])
+
   return (
     <>
-
-
+    <h1> Web Football </h1>
+    {
+      standings.length > 0 ? standings.map((val, i) => ( <Item data={val}/> )) : "loading"
+    }
     </>
   )
 }
---
+++ web-football/src/item.jsx
@@ -0,0 +1,18 @@
+const TEST_TEAM = {
+    abbreviation : "MAN", 
+    displayName : "Manchester United", 
+    id : "360", 
+    location : "Manchester United", 
+    logos : [{href: 'https://a.espncdn.com/i/teamlogos/soccer/500/360.png'}],
+    name : "Manchester United", 
+    shortDisplayName : "Man United", 
+}
+
+
+export default function Item( {pos, data} ) {
+    return (
+        <>
+        <h1> hi</h1>
+        </>
+    )
+}
